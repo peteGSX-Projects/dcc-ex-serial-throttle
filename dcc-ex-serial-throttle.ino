d996fdb95e36a94a4bebca476262f8303b8bf501
peteGSX <peteracole@outlook.com.au>
2022-07-29 16:21:58 +1000

    Start on commands

--- dcc-ex-serial-throttle.ino
+++ dcc-ex-serial-throttle.ino
@@ -11,3 +11,46 @@ void setup() {
 void loop() {
 
 }
+
+void setTrackPower(bool powerState) {
+  Serial.print(F("<"));
+  Serial.print(powerState);
+  Serial.print(">");
+}
+
+void joinProgMain() {
+  Serial.print(F("<1 JOIN>"));
+}
+
+void throttleLoco(uint16_t locoAddress, int8_t speed, bool direction) {
+  if (locoAddress < 1 || locoAddress > 10293) {
+    return;
+  }
+  if (speed < -1 || speed > 126) {
+    return;
+  }
+  Serial.print(F("<t 1 "));
+  Serial.print(locoAddress);
+  Serial.print(F(" "));
+  Serial.print(speed);
+  Serial.print(F(" "));
+  Serial.print(direction);
+  Serial.print(F(">"));
+}
+
+void forgetAllLocos() {
+  Serial.print(F("<->")):
+}
+
+void forgetLoco(uint16_t locoAddress) {
+  if (locoAddress < 1 || locoAddress > 10293) {
+    return;
+  }
+  Serial.print(F("<- "));
+  Serial.print(locoAddress);
+  Serial.print(F(">"));
+}
+
+void eStopAll() {
+  Serial.print(F("<!>"));
+}
