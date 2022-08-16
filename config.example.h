e3abc9df02707903f7d04f201a3dfeb6384ec57a
peteGSX <peteracole@outlook.com.au>
2022-08-17 07:41:44 +1000

    Ugly but pots working

--- config.example.h
+++ config.example.h
@@ -5,13 +5,17 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-/*
-Comment this line out to force the rotary encoder to read full steps only.
-*/
-#define HALF_STEP
+#define SAMPLES 30
 
 #define USE_OLED
 #define OLED_ADDRESS 0x3C
 #define OLED_FONT System5x7
+
+/////////////////////////////////////////////////////////////////////////////////////
+//  OLED.
+// 
+#define CS_PIN  7
+// #define RST_PIN 8
+#define DC_PIN  9
 
 #endif
--- dcc-ex-serial-throttle.ino
+++ dcc-ex-serial-throttle.ino
@@ -34,38 +34,118 @@
 
 // If using OLED, include the libraries and instantiate the object
 #ifdef USE_OLED
+#include <SPI.h>
 #include "SSD1306Ascii.h"
-#include "SSD1306AsciiAvrI2c.h"
-SSD1306AsciiAvrI2c oled;
+#include "SSD1306AsciiSpi.h"
+SSD1306AsciiSpi oled;
 #endif
 
 AnalogueAverage pot1(A0);
 AnalogueAverage pot2(A1);
 
+int8_t loco1Speed = 0;
+int8_t loco2Speed = 0;
+bool loco1Direction = 1;
+bool loco2Direction = 1;
+
 void setup() {
   Serial.begin(115200);
+  oled.begin(&SH1106_128x64, CS_PIN, DC_PIN);
+  oled.setFont(Callibri11);
+  oled.clear();
 }
 
 void loop() {
-  pot1.averageInput();
-  pot2.averageInput();
-  Serial.print(F("Pot 1/2 average: "));
-  Serial.print(pot1.getAverage());
-  Serial.print(F("/"));
-  Serial.println(pot2.getAverage());
+
+  processSliders();
+  
 }
 
-#ifdef USE_OLED
 /*
-OLED Functions below here if OLED in use.
+Throttle input functions here
 */
 
-// This function updates the OLED with the provided text at the cursor position
-void oledUpdate(int column, int row, String textToPrint) {
-  char textChar[textToPrint.length() + 1];
-  textToPrint.toCharArray(textChar, textToPrint.length() + 1);
-  oled.setCursor(column, row);
-  oled.clearToEOL();
-  oled.write(textChar);
+/*
+Function to process the potentiometer inputs to control speed
+*/
+void processSliders() {
+  pot1.averageInput();
+  pot2.averageInput();
+  bool updateDisplay = false;
+  uint16_t newPot1 = pot1.getAverage();
+  uint16_t newPot2 = pot2.getAverage();
+  int8_t newL1Speed;
+  int8_t newL2Speed;
+  bool newL1Direction;
+  bool newL2Direction;
+  if (newPot1 > 520) {
+    newL1Speed = map(pot1.getAverage(), 521, 1020, 0, 126);
+    newL1Direction = 1;
+  } else if (newPot1 < 500) {
+    newL1Speed = map(pot1.getAverage(), 499, 3, 0, 126);
+    newL1Direction = 0;
+  } else if (newPot1 >= 510) {
+    newL1Speed = 0;
+    newL1Direction = 1;
+  } else if (newPot1 < 510) {
+    newL1Speed = 0;
+    newL1Direction = 0;
+  }
+  if (newPot2 > 520) {
+    newL2Speed = map(pot2.getAverage(), 521, 1020, 0, 126);
+    newL2Direction = 1;
+  } else if (newPot2 < 500) {
+    newL2Speed = map(pot2.getAverage(), 499, 3, 0, 126);
+    newL2Direction = 0;
+  } else if (newPot2 >= 510) {
+    newL2Speed = 0;
+    newL2Direction = 1;
+  } else if (newPot2 < 510) {
+    newL2Direction = 0;
+    newL2Speed = 0;
+  }
+  if (newL1Speed != loco1Speed || newL1Direction != loco1Direction) {
+    loco1Speed = newL1Speed;
+    loco1Direction = newL1Direction;
+    updateDisplay = true;
+  }
+  if (newL2Speed != loco2Speed || newL2Direction != loco2Direction) {
+    loco2Speed = newL2Speed;
+    loco2Direction = newL2Direction;
+    updateDisplay = true;
+  }
+  if (updateDisplay) {
+    Serial.print(F("Loco 1/2 speed/direction: "));
+    Serial.print(loco1Speed);
+    Serial.print(F("/"));
+    Serial.print(loco1Direction);
+    Serial.print(F("/"));
+    Serial.print(loco2Speed);
+    Serial.print(F("/"));
+    Serial.println(loco2Direction);
+    displaySpeeds();
+  }
 }
-#endif
+
+/*
+OLED Functions below
+*/
+void displaySpeeds() {
+  oled.setCursor(10, 0);
+  oled.set2X();
+  if (loco1Direction) {
+    oled.print(F("F"));
+  } else {
+    oled.print(F("R"));
+  }
+  oled.print(loco1Speed);
+  oled.clearToEOL();
+  oled.setCursor(74, 0);
+  if (loco2Direction) {
+    oled.print(F("F"));
+  } else {
+    oled.print(F("R"));
+  }
+  oled.print(loco2Speed);
+  oled.clearToEOL();
+}
