692a31143850defd2541f0cbca7e4d0519b5599a
peteGSX <peteracole@outlook.com.au>
2022-08-24 11:09:55 +1000

    Add usage details

--- AnalogueAverage.h
+++ AnalogueAverage.h
@@ -17,6 +17,42 @@
  *  along with this code.  If not, see <https://www.gnu.org/licenses/>.
 */
 
+/*
+ *  How to use this class
+ *
+ * This class simply reads an analogue input pin on an Arduino and calculates
+ * the moving average across the defined number of samples.
+ * 
+ * This can be used to calculate the average of multiple analogue inputs by
+ * instantiating multiple objects, however all will be averaged across the
+ * same number of samples.
+ * 
+ * Include the class with:
+ * 
+ * #include "AnalogueAverage.h"
+ * 
+ * The default number of samples is 20, but this can be overridden at compile
+ * time by using:
+ * 
+ * #define SAMPLES XX.
+ * 
+ * Instantiate each object with:
+ * 
+ * AnalogeAverage input(pin)
+ * 
+ * To ensure continued averaging, you must call the averageInput() function
+ * continuously within your main loop() function:
+ * 
+ * void loop() {
+ *   input.averageInput();
+ *   ...
+ * }
+ * 
+ * To obtain the current moving object, call the getAverage() function:
+ * 
+ * uint16_t movingAverage = input.getAverage();
+*/
+
 #ifndef ANALOGAVERAGE_H
 #define ANALOGAVERAGE_H
 
