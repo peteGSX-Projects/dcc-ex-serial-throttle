f4fe799fe31ed66fdfde75a21c856e42a07d15be
peteGSX <peteracole@outlook.com.au>
2023-10-07 04:54:08 +1000

    Update version

--- version.h
+++ version.h
@@ -1,7 +1,8 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define VERSION "0.0.3"
+#define VERSION "0.0.4"
+// 0.0.4  - Menu becoming functional and receiving CS broadcasts
 // 0.0.3  - Communicating with CS using DCCEXProtocol library
 // 0.0.2  - Updated to basic throttle control using DCCEXProtocol library
 // 0.0.1  - Basic throttle control with hard coded loco address and minimal functions
